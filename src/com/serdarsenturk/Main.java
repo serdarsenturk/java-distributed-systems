082ccdfea9aa666d388e3078a0127d091a01a6ca
Serdar <serdarsenturk@windowslive.con>
2020-04-26 19:54:03 +0300

    Update and Add comments

--- src/com/serdarsenturk/Main.java
+++ src/com/serdarsenturk/Main.java
@@ -1,17 +1,30 @@
 package com.serdarsenturk;
 import com.serdarsenturk.Movie;
+
+import java.security.KeyException;
 import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
         var myMovie = new Movie("Engin", Genre.Action);
-
         myMovie.setPrice(100);
+        try {
+            myMovie.addComment(new Comment(1, "A", 101, new Date()));
+            myMovie.addComment(new Comment(2, "B", 102, new Date()));
+            myMovie.addComment(new Comment(3, "C", 103, new Date()));
+            myMovie.addComment(new Comment(4, "D", 104, new Date()));
+            myMovie.addComment(new Comment(4, "E", 105, new Date()));
+        }
+        catch (KeyException ex) {
+            System.out.println(ex.getMessage());
+        }
 
-        myMovie.addComment(1, "First comment", 123);
-        myMovie.addComment(2, "Second comment", 124);
+
+        //System.out.println(myMovie.getComments("A"));
         System.out.println(myMovie.getName());
         System.out.println("Comment count: " + myMovie.getCommentSize());
+
+
         for(var comment : myMovie.getComments()){
             System.out.println("Comment Id: " + comment.getId());
             System.out.println("Comment Text: " + comment.getComment());
@@ -19,6 +32,7 @@ public class Main {
             System.out.println("Comment Date: " + comment.getCreatedDate());
         }
         System.out.println(Genre.Action.ordinal());
+
 //        Movie t1 = new Movie();
 //
 //        List<String> actors1 = new ArrayList();
--- src/com/serdarsenturk/Movie.java
+++ src/com/serdarsenturk/Movie.java
@@ -1,42 +1,61 @@
 package com.serdarsenturk;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Hashtable;
+import java.security.KeyException;
+import java.util.*;
 
 public class Movie{
 
     private String name;
     private int price;
-    private Hashtable<Integer, ArrayList<comment>> comments;
+    private HashMap<Integer, Comment> comments;
     private Genre genre;
 
     public Movie(String name, Genre genre){
         this.name = name;
         this.genre = genre;
-        this.comments = new Hashtable<Integer, ArrayList<comment>>();
+        this.comments = new HashMap<>();
     }
+
     public String getName(){
         return this.name;
     }
+
     public int getPrice(){
         return price;
     }
+
     public int getCommentSize(){
         return this.comments.size();
     }
+
     public Iterable<Comment> getComments(){
-        return this.comments;
+        var commentsDictValues = this.comments.values();
+        var commentArray = new ArrayList<>(commentsDictValues);
+
+        return commentArray;
     }
+
+    public void addComment(Comment comment) throws KeyException {
+        if(this.comments.containsKey(comment.getId())){
+            throw new KeyException("Key zaten var");
+        }
+
+        this.comments.put(comment.getId(), comment);
+    }
+
+    public void updateComment(Comment comment) throws KeyException {
+        if(!this.comments.containsKey(comment.getId())){
+            throw new KeyException("Key yok");
+        }
+
+        this.comments.put(comment.getId(), comment);
+    }
+
     public void setPrice(int price){
         this.price = price;
     }
+
     public int getNextMinimumPrice(){
         return this.price + this.price * 10 / 100;
-    }
-    public void addComment(int id, String commentText, int userId){
-        var comment = new Comment(id, commentText, userId, new Date());
-
-        this.comments;
     }
 }
