f6755ae8c48325274c137691d81d68e45fbbceb7
Serdar <serdarsenturk@windowslive.con>
2020-04-27 00:32:03 +0300

    Do getter and add methods for actor and movie class

--- src/com/serdarsenturk/Actor.java
+++ src/com/serdarsenturk/Actor.java
@@ -1,16 +1,19 @@
 package com.serdarsenturk;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Actor {
     private int actorId;
     private String name;
     private int age;
+    private HashMap<String, Movie> movies;
 
     public Actor(int actorId, String name, int age){
         this.actorId = actorId;
         this.name = name;
         this.age = age;
+        this.movies = new HashMap<>();
     }
 
     public String getName(){
@@ -23,5 +26,14 @@ public class Actor {
 
     public int getActorId(){
         return this.actorId;
+    }
+
+    public Iterable<Movie> getMovies(){
+        var actorsDicValues = this.movies.values();
+        return new ArrayList<>(actorsDicValues);
+    }
+
+    public void addMovies(Movie movie){
+        this.movies.put(movie.getName(), movie);
     }
 }
--- src/com/serdarsenturk/Main.java
+++ src/com/serdarsenturk/Main.java
@@ -6,6 +6,21 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+
+        var movie1 = new Movie("A", Genre.Action);
+        var actor1 = new Actor(1, "Serdar", 22);
+        var actor2 = new Actor(2, "Engin", 31);
+
+        try {
+            movie1.addActor(actor1);
+            movie1.addActor(actor2);
+            actor1.addMovies(movie1);
+            actor2.addMovies(movie1);
+        }
+        catch (KeyException ex){
+            System.out.println(ex.getMessage());
+        }
+/*
         var myMovie = new Movie("Engin", Genre.Action);
 
 
@@ -17,6 +32,7 @@ public class Main {
             myMovie.addComment(new Comment(4, "D", 104, new Date()));
             myMovie.updateComment(new Comment(2, "K", 105, new Date()));
             myMovie.addActor(new Actor(900, "Serdar", 22));
+
 
         }
         catch (KeyException ex) {
@@ -42,7 +58,7 @@ public class Main {
 
         }
         System.out.println(Genre.Action.ordinal());
-
+*/
 //        Movie t1 = new Movie();
 //
 //        List<String> actors1 = new ArrayList();
