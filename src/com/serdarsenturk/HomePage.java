731c97015194c872ef61cbb148a60e105cbbf3f9
Serdar <serdarsenturk@windowslive.con>
2020-08-23 03:52:23 +0300

    Add query parameter to DummyMovieService

--- src/com/serdarsenturk/HomePage.java
+++ src/com/serdarsenturk/HomePage.java
@@ -1,14 +1,14 @@
-package com.serdarsenturk;
-
-public class HomePage {
-
-    public static void movieDetails(int id){
-        System.out.println("Welcome to MovieDb");
-        var movie = new DummyMovieService();
-        movie.getById(id);
-    }
-
-    public static void addComment(int id){
-
-    }
-}
+//package com.serdarsenturk;
+//
+//public class HomePage {
+//
+//    public static void movieDetails(int id){
+//        System.out.println("Welcome to MovieDb");
+//        var movie = new DummyMovieService(String query);
+//        movie.getById(id);
+//    }
+//
+//    public static void addComment(int id){
+//
+//    }
+//}
